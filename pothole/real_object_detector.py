f4d9e0d0e373692c38178a462c8550c73ffc0cae
arunabh-alt <arunabhbora.ab@gmail.com>
2024-01-07 21:05:11 +0000

    update

--- pothole/real_object_detector.py
+++ pothole/real_object_detector.py
@@ -49,7 +49,7 @@ class ContourDetectionNode(Node):
 
     def detect_magenta_contours(self, image):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower_magenta = np.array([0, 0, 47])
+        lower_magenta = np.array([0, 0, 42])
         upper_magenta = np.array([15, 15, 57])
         mask = cv2.inRange(hsv, lower_magenta, upper_magenta)
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
